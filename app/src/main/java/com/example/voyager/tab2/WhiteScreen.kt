bcd7ab6d9c8fcd9b171f2bf4c8c2061c66bb701b
Mateusz Kowol <mateusz.kowol@euvic.pl>
2023-11-13 12:37:35 +0100

    add navigation

--- app/src/main/java/com/example/voyager/tab2/WhiteScreen.kt
+++ app/src/main/java/com/example/voyager/tab2/WhiteScreen.kt
@@ -2,12 +2,17 @@ package com.example.voyager.tab2
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import com.example.voyager.YellowScreen
 
 class WhiteScreen: Screen {
     @Composable
@@ -18,7 +23,11 @@ class WhiteScreen: Screen {
 
 @Composable
 private fun Screen() {
-    Box(modifier = Modifier.background(Color.White)) {
+    val navigator = LocalNavigator.currentOrThrow.parent!!.parent!!
+    Column(modifier = Modifier.background(Color.White)) {
+        Button(onClick = { navigator.push(YellowScreen()) }) {
+            Text(text = "click me")
+        }
         Text(text = "White Screen", fontSize = 40.sp)
     }
 }
